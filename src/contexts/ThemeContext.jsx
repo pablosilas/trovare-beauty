a518fe41d0e40a6da72940a8651b431ae88fa545
p.siqueira <p.siqueira@cigamgestor.com.br>
2026-03-30 01:18:37 -0300

    mudança no tema default

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -4,7 +4,7 @@ const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("barberos-theme") || "dark";
+    return localStorage.getItem("barberos-theme") || "light";
   });
 
   useEffect(() => {
